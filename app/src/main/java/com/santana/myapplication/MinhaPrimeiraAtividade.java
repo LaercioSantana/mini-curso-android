53d664b1e3b23695136594a58555431da5e10e65
Laercio Santana <laerciosantana021@hotmail.com>
2016-07-29 23:03:38 -0300

    hello world

---
+++ app/src/main/java/com/santana/myapplication/MinhaPrimeiraAtividade.java
@@ -0,0 +1,13 @@
+package com.santana.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MinhaPrimeiraAtividade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minha_primeira_atividade);
+    }
+}
