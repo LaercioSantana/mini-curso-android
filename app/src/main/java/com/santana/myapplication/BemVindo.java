a6cef2dad111f36a05b525a06545744315d69186
Laercio Santana <laerciosantana021@hotmail.com>
2016-09-08 23:46:20 -0300

    tela bem-vindo

--- app/src/main/java/com/santana/myapplication/BemVindo.java
+++ app/src/main/java/com/santana/myapplication/BemVindo.java
@@ -3,11 +3,11 @@ package com.santana.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MinhaPrimeiraAtividade extends AppCompatActivity {
+public class BemVindo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_minha_primeira_atividade);
+        setContentView(R.layout.bem_vindo);
     }
 }
